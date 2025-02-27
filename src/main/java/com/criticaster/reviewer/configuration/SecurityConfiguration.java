7ec92e46d374096e0b9ed42a71ab94fb66adaba5
Боровов Максим Сергеевич <maxim.borovov@corp.codeinside.ru>
2025-02-27 17:42:26 +0300

    openapi draft

--- src/main/java/com/criticaster/reviewer/configuration/SecurityConfiguration.java
+++ src/main/java/com/criticaster/reviewer/configuration/SecurityConfiguration.java
@@ -2,17 +2,41 @@ package com.criticaster.reviewer.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
   @Bean
-  public WebSecurityCustomizer webSecurityCustomizer() {
-    return (web) -> web.ignoring()
-        .requestMatchers(new AntPathRequestMatcher("/**"));
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/cities", "/api/cities/**", "/api/restaurants/**/menu").permitAll() // Открытые эндпоинты
+            .requestMatchers("/api/restaurants/**/reviews").authenticated() // Отзывы только для авторизованных
+            .requestMatchers(HttpMethod.POST, "/api/reviews/**").hasRole("CUSTOMER") // Оставлять отзывы могут только клиенты
+            .requestMatchers(HttpMethod.POST, "/api/restaurants/**/menu").hasRole("OWNER") // Владельцы управляют меню
+            .requestMatchers(HttpMethod.DELETE, "/api/reviews/**").hasRole("MODERATOR") // Модератор может удалять отзывы
+            .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    return http.build();
+  }
+
+  @Bean
+  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+    grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+    return jwtAuthenticationConverter;
   }
 }
